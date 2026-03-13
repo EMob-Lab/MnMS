0b05ddf7a6304966154b44ef608360ffc8f18e75
Cecile Becarie <cecile.becarie@univ-eiffel.fr>
2026-03-13 17:32:23 +0100

    Update MnMS_outputs_animation.py

--- script/visualization/MnMS_outputs_animation.py
+++ script/visualization/MnMS_outputs_animation.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: cecile.becarie, christophe.tettarassar
+@author: cecile.becarie
 """
 
 import os
@@ -42,19 +42,20 @@ def timedelta_to_time(total_secondes):
 
 
 def update(n):
-    time_current = time_init + (flow_dt * n)
+
+    time_current = start + (flow_dt * n)
     time_step_mn = time_mn_init + int(flow_dt.total_seconds()) * n
     time_text.set_text(time_current.strftime(time_template))
     print(n, time_current.strftime(time_template))
 
     # Users
-    #if time_current.strftime("%H:%M:%S.00") in df_grouped_users.groups.keys():
-    #    df_current_users = df_grouped_users.get_group(time_current.strftime("%H:%M:%S.00"))
-    #    print('users ',len(df_current_users))
-    #    arr_current_users = df_current_users[['X', 'Y']].copy().to_numpy()
-    #    scat_users.set_offsets(arr_current_users)
+    if time_current.strftime("%H:%M:%S.00") in df_grouped_users.groups.keys():
+        df_current_users = df_grouped_users.get_group(time_current.strftime("%H:%M:%S.00"))
+        print('users ',len(df_current_users))
+        arr_current_users = df_current_users[['X', 'Y']].copy().to_numpy()
+        scat_users.set_offsets(arr_current_users)
 
-    #    user_number_text.set_text('# users: ' + str(len(df_current_users['ID'].unique())))
+        user_number_text.set_text('# users: ' + str(len(df_current_users['ID'].unique())))
 
     # Vehicles
     if time_current.strftime("%H:%M:%S.00") in df_grouped_vehs.groups.keys():
@@ -68,7 +69,7 @@ def update(n):
 
         veh_number_text.set_text('# vehicles: ' + str(len(cv)))
 
-    return scat_vehs, #scat_users,
+    return scat_vehs, scat_users,
 
 
 # Custom argparse type to check if path is valid file
@@ -96,13 +97,19 @@ if __name__ == '__main__':
     vehicles_file = args.vehicles_file
     users_file = args.users_file
 
-    flow_dt = timedelta(seconds=1)
-    simulation_duration = 3600
-    time_init = datetime(2023, 12, 14, 6, 30, 0)
+    # Time parameters
+    df_vehs = pd.read_csv(vehicles_file, sep=';')
+    start = datetime.strptime(df_vehs['TIME'].iloc[0], "%H:%M:%S.%f")
+    end = datetime.strptime(df_vehs['TIME'].iloc[-1], "%H:%M:%S.%f")
+    simulation_duration = (end - start).total_seconds()
+
+    diff = df_vehs['TIME'] != df_vehs['TIME'].shift()
+    diff.iloc[0] = False
+    flow_dt = datetime.strptime(df_vehs.loc[diff.idxmax()]['TIME'], "%H:%M:%S.%f") - start
 
     # Plot setting
-    color_vehs = {'STOP': 'green', 'PICKUP': 'orange', 'SERVING': 'blue', 'PV': 'red', 'REPOSITIONING':'grey'}
-    color_veh_type = {'Bus': 'green', 'Tram': 'magenta', 'Metro': 'red'}
+    color_vehs = {'STOP': 'green', 'PICKUP': 'orange', 'SERVING': 'red', 'PV': 'blue', 'REPOSITIONING':'grey'}
+    color_veh_type = {'Bus': 'purle', 'Tram': 'magenta', 'Metro': 'yellow'}
 
     # Load the network json file
     with open(network_file, 'r') as f:
@@ -153,10 +160,10 @@ if __name__ == '__main__':
             stopsY[layer['ID']] = stops_Y
 
     # Build a GeoDataFrame for the road network
-    gdf_roads = gpd.GeoDataFrame(
-        geometry=[LineString(line) for line in lines_car],
-        crs="EPSG:2154"  # Lambert 93
-    ).to_crs(epsg=3857)  # Convert to Web Mercator
+    #gdf_roads = gpd.GeoDataFrame(
+    #    geometry=[LineString(line) for line in lines_car],
+    #    crs="EPSG:2154"  # Lambert 93
+    #).to_crs(epsg=3857)  # Convert to Web Mercator
 
     # Vehicles
     df_vehs = pd.read_csv(vehicles_file, sep=';')
@@ -206,63 +213,64 @@ if __name__ == '__main__':
 
     #df_vehs.to_csv("vehs_ext.csv", index=False, sep=';')
 
-    time_mn_init = time_init.hour * 3600 + time_init.minute * 60
+    time_mn_init = start.hour * 3600 + start.minute * 60
 
     df_vehs['sep'] = df_vehs['POSITION'].str.find(' ')
     df_vehs['X'] = df_vehs.apply(lambda x : float(x['POSITION'][:x['sep']]), axis=1)
     df_vehs['Y'] = df_vehs.apply(lambda x : float(x['POSITION'][x['sep']+1:]), axis=1)
-    df_vehs['X'], df_vehs['Y'] = project_to_webmercator(df_vehs['X'], df_vehs['Y'])
+    #df_vehs['X'], df_vehs['Y'] = project_to_webmercator(df_vehs['X'], df_vehs['Y'])
 
     df_grouped_vehs = df_vehs.groupby('TIME')
 
     # Users
-    #df_users = pd.read_csv(users_file, sep=';')
+    df_users = pd.read_csv(users_file, sep=';')
 
-    #df_users = df_users.drop(df_users[df_users.STATE == 'DEADEND'].index)
+    df_users = df_users.drop(df_users[df_users.STATE == 'DEADEND'].index)
 
-    #df_users['TIME_STEP'] = df_users.apply(lambda x: datetime.strptime(x['TIME'], "%H:%M:%S.%f").hour * 3600 + (
-     #   datetime.strptime(x['TIME'], "%H:%M:%S.%f").minute) * 60 + datetime.strptime(x['TIME'], "%H:%M:%S.%f").second,
-      #                                     axis=1)
+    df_users['TIME_STEP'] = df_users.apply(lambda x: datetime.strptime(x['TIME'], "%H:%M:%S.%f").hour * 3600 + (
+       datetime.strptime(x['TIME'], "%H:%M:%S.%f").minute) * 60 + datetime.strptime(x['TIME'], "%H:%M:%S.%f").second,
+                                         axis=1)
 
     # Complete data for each missing time step
-    #df_grouped_users_by_id = df_users.groupby('ID')
-    # df_grouped_users_by_id=None
-    # new_rows = []
-    # for id, group in df_grouped_users_by_id:
-    #
-    #     print('user ', id)
-    #     group.sort_values(by=['TIME'])
-    #
-    #     if len(group[group.STATE == 'WAITING_ANSWER']) > 0:
-    #         d1 = group[group.STATE == 'WAITING_ANSWER'].iloc[0].TIME_STEP
-    #         d2 = d1
-    #         if d1 > 0:
-    #             if len(group[group.STATE == 'INSIDE_VEHICLE']) > 0:
-    #                 d2 = group[group.STATE == 'INSIDE_VEHICLE'].iloc[0].TIME_STEP
-    #             else:
-    #                 if len(group[group.STATE == 'STOP']):
-    #                     d2 = group[group.STATE == 'STOP'].iloc[0].TIME_STEP
-    #
-    #             if d2 - d1 > flow_dt.total_seconds():
-    #                 for n in range(1, d2 - d1):
-    #                     new_row = group[group.STATE == 'WAITING_ANSWER'].iloc[0]
-    #                     new_row.TIME_STEP = group[group.STATE == 'WAITING_ANSWER'].iloc[0].TIME_STEP+n*flow_dt.total_seconds()
-    #                     new_row.TIME = timedelta_to_time(new_row.TIME_STEP).strftime("%H:%M:%S.00")
-    #                     new_rows.append(new_row)
-    #
-    # df_new_rows = pd.DataFrame(new_rows)
-    # df_users = pd.concat([df_users, pd.DataFrame(df_new_rows)], ignore_index=True)
-    #
-    # df_users=df_users.sort_values(by=['TIME'])
+    df_grouped_users_by_id = df_users.groupby('ID')
 
-    # df_users['sep'] = df_users['POSITION'].str.find(' ')
-    # df_users['X'] = df_users.apply(lambda x: float(x['POSITION'][:x['sep']]), axis=1)
-    # df_users['Y'] = df_users.apply(lambda x: float(x['POSITION'][x['sep'] + 1:]), axis=1)
-    # df_users['X'], df_users['Y'] = project_to_webmercator(df_users['X'], df_users['Y'])
+    new_rows = []
+
+    for id, group in df_grouped_users_by_id:
+
+         print('user ', id)
+         group.sort_values(by=['TIME'])
+
+         if len(group[group.STATE == 'WAITING_ANSWER']) > 0:
+             d1 = group[group.STATE == 'WAITING_ANSWER'].iloc[0].TIME_STEP
+             d2 = d1
+             if d1 > 0:
+                 if len(group[group.STATE == 'INSIDE_VEHICLE']) > 0:
+                     d2 = group[group.STATE == 'INSIDE_VEHICLE'].iloc[0].TIME_STEP
+                 else:
+                     if len(group[group.STATE == 'STOP']):
+                         d2 = group[group.STATE == 'STOP'].iloc[0].TIME_STEP
+
+                 if d2 - d1 > flow_dt.total_seconds():
+                     for n in range(1, d2 - d1):
+                         new_row = group[group.STATE == 'WAITING_ANSWER'].iloc[0]
+                         new_row.TIME_STEP = group[group.STATE == 'WAITING_ANSWER'].iloc[0].TIME_STEP+n*flow_dt.total_seconds()
+                         new_row.TIME = timedelta_to_time(new_row.TIME_STEP).strftime("%H:%M:%S.00")
+                         new_rows.append(new_row)
+
+    df_new_rows = pd.DataFrame(new_rows)
+    df_users = pd.concat([df_users, pd.DataFrame(df_new_rows)], ignore_index=True)
+
+    df_users=df_users.sort_values(by=['TIME'])
+
+    df_users['sep'] = df_users['POSITION'].str.find(' ')
+    df_users['X'] = df_users.apply(lambda x: float(x['POSITION'][:x['sep']]), axis=1)
+    df_users['Y'] = df_users.apply(lambda x: float(x['POSITION'][x['sep'] + 1:]), axis=1)
+    #df_users['X'], df_users['Y'] = project_to_webmercator(df_users['X'], df_users['Y'])
+
+    df_users['TIME_STEP'] = df_users.apply(lambda x: datetime.strptime(x['TIME'], "%H:%M:%S.%f").hour * 3600 + (datetime.strptime(x['TIME'], "%H:%M:%S.%f").minute) * 60 + datetime.strptime(x['TIME'], "%H:%M:%S.%f").second, axis = 1)
     #
-    # df_users['TIME_STEP'] = df_users.apply(lambda x: datetime.strptime(x['TIME'], "%H:%M:%S.%f").hour * 3600 + (datetime.strptime(x['TIME'], "%H:%M:%S.%f").minute) * 60 + datetime.strptime(x['TIME'], "%H:%M:%S.%f").second, axis = 1)
-    # #
-    # df_grouped_users = df_users.groupby('TIME')
+    df_grouped_users = df_users.groupby('TIME')
 
     # Plot
     figsize = (24, 9)
@@ -271,11 +279,11 @@ if __name__ == '__main__':
     ax1 = fig.add_subplot(1, 1, 1)
 
     # Plot the road network first
-    gdf_roads.plot(ax=ax1, color='grey', linewidth=1, label='road network')
+    #gdf_roads.plot(ax=ax1, color='grey', linewidth=1, label='road network')
 
     # Add the OpenStreetMap basemap
-    if args.osm:
-        ctx.add_basemap(ax1, crs=gdf_roads.crs, source=ctx.providers.OpenStreetMap.Mapnik)
+    #if args.osm:
+    #    ctx.add_basemap(ax1, crs=gdf_roads.crs, source=ctx.providers.OpenStreetMap.Mapnik)
 
     lc_car = LineCollection(lines_car, linewidths=1)
     lc_car.set_color('grey')
@@ -301,16 +309,17 @@ if __name__ == '__main__':
 
     # Timer
     time_template = "%H:%M:%S"
-    time_text = ax1.text(0.4, -0.05, time_init.strftime(time_template), transform=ax1.transAxes, fontsize=18)
+    time_text = ax1.text(0.4, -0.05, start.strftime(time_template), transform=ax1.transAxes, fontsize=18)
     time_text.set_bbox(dict(facecolor='grey', alpha=0.5))
-    time_text.set_text(time_init.strftime(time_template))
+    time_text.set_text(start.strftime(time_template))
 
     # Vehicle and user number display
     veh_number_text = ax1.text(0.6, -0.05, '# vehicles: 0', transform=ax1.transAxes, fontsize=16)
     user_number_text = ax1.text(0.6, -0.12, '# users: 0', transform=ax1.transAxes, fontsize=16)
 
     # Vehicles
-    scat_vehs = ax1.scatter([], [], c='black', s=30, marker='h',alpha=0.8)
+    scat_vehs = ax1.scatter([], [], c='blue', s=30, marker='h',alpha=0.8)
+
     # Only for legend
     PV_legend = ax1.scatter([], [], c=color_vehs['PV'], s=50, marker='h',alpha=0.8, label='Personal vehicle')
     OnDemand_STOP_legend = ax1.scatter([], [], c=color_vehs['STOP'], s=50, marker='h', alpha=0.8, label='On demand vehicle waiting')
@@ -321,13 +330,13 @@ if __name__ == '__main__':
 
 
     # Users
-    #scat_users = ax1.scatter([], [], c='red', s=10, marker='x', label='User')
+    scat_users = ax1.scatter([], [], c='red', s=50, marker='x', label='User')
 
     plt.legend(handles=[scat_users, PV_legend, OnDemand_STOP_legend, OnDemand_PICKUP_legend, OnDemand_SERVING_legend], loc=(0.8, 0.8))
 
-    animALL = FuncAnimation(fig, update, frames=simulation_duration+1, interval=100, repeat=False, blit=False)
+    animALL = FuncAnimation(fig, update, frames=int(simulation_duration)+1, interval=100, repeat=False, blit=False)
 
-    backup = True
+    backup = False
     if backup:
         aniWriter = mpl.animation.writers['ffmpeg']
         aniWriter = aniWriter(fps=5)
